8734bf4de6f88d27f3b5a65fb016d28f3810bfed
brianru <brianrubinton@gmail.com>
2013-03-08 07:59:11 -0700

    begun implementing visitor pattern for compiler and printer functions

--- brevity2.py
+++ brevity2.py
@@ -31,12 +31,13 @@ class socket(object):
 	self.variables = variables
         self.linked_node = linked_node
 
-    def __str__(self)
+    def __str__(self):
         return 'COmponent type: %s \n
 		Text: %s \n
 		Default variables: %s \n
 		Linked node? %s \n' % (type(self), self.text, self.variables, if(linked_node))
-
+    def accept(self, visitor):
+        visitor.visit_socket(self)
     def linkNode(self, new_node):
 	"""Attempts to update the linked node.
 	Does not raise an exception if this is replacing an existing linked node.
@@ -46,7 +47,9 @@ class socket(object):
 	node_vars = []
 	for x in new_node.sockets:
 	    node_vars.append(x.variables.keys())
-	if self.variables.issubset(node_vars):
+	#build separate method in socket to perform below comparison
+	#if new_node >= self
+	if self.variables.issubset(node_vars): 
 	    linked_node = new_node
 	    return True
         return False
@@ -60,10 +63,11 @@ class node(object):
     def __init__(self, sockets):
 	self.sockets = sockets
 	#raise exception if 1) sockets does not contain only sockets or 2) sockets is empty
-
     def __str__(self):
 	return 'Component type: %s \n
 	        Number of sockets: %d' % (type(self), len(sockets))
+    def accept(self, visitor):
+	visitor.visit_node(self)
     
 class document(object):
     """Top structure. Contains instance variables. Separates document components from particular document instance.
@@ -78,6 +82,9 @@ class document(object):
 	self.nodes = nodes
 	self.variables = variables
 
+    def accept(self, visitor):
+	visitor.visit_document(self)
+
     cached_compile = None
     cached_vars = None
 
@@ -90,31 +97,50 @@ class document(object):
 class docIterator(object):
     """Iterate through document in most efficient manner. """
 
-class compiler(object):
+class visitor(object):
+    """Abstract class defines interface for visitor classes."""
+    def visit_socket(self, socket):
+	pass
+    def visit_node(self, node):
+	pass
+    def visit_document(self, document):
+	pass
+
+class compiler(visitor):
     """Use docIterator to aggregate active objects and output formatted object.
     STRATEGY for different raw formats (txt, md, tex)
     
     """
+    def __init__(self):
+        result_variables = dict()
+        result_text = ''
 
-class printer(object):
+    def visit_document(self, document):
+	result_variables = document.variables
+	for x in document.nodes:
+	    x.accept(self)
+    
+    def visit_node(self, node):
+	for y in node.sockets:
+	    y.accept(self)
+
+    def visit_socket(self, socket):
+	if socket.linked_node:
+            socket.linked_node.accept(self)
+	else:
+	    result_text.append('/n' + socket.text)
+	    #this check should only be adding defaults not redefined in doc instance
+	    #refactor document construction methods if otherwise
+	    for j in socket.variables.keys()
+	        if j not in result_variables.keys():
+		    result_variables.update(j, y.variables[j])
+
+class printer(visitor):
     """Output raw object with variable placeholders and values.
 
     """
-
-class new(object):
-    """CLI to create objects.
-
-    """
-
-class br_controller(object):
-    """Primary controller class.
-
-    """
-
-class list(object):
-    """Display cached (working) objects.
-
-    """ 
+    def __init__(self):
+        result_text = ''
 
 class us_constitution_static_test(unittest.TestCase):
     """Import US constitution (cumulative of all amendments) from a prepared .brvty file.
