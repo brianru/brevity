71c39c9773bfad3d1c13186edbb9c0d522ee9166
brianru <brianrubinton@gmail.com>
2013-03-30 20:31:11 -0400

    Refactoring test script.

---
+++ brevity/tests2.py
@@ -0,0 +1,48 @@
+import os
+import pdb
+import unittest
+import xml.etree.ElementTree as etree
+import application as br
+
+
+##### UNIT TESTS #####
+
+class BadInputTestCase(unittest.TestCase):
+    pass
+
+
+class IncompatibleComponentTestCase(unittest.TestCase):
+    pass
+
+
+class DocumentConstructionTestCase(unittest.TestCase):
+    pass
+
+
+class DataTraversalTestCase(unittest.TestCase):
+    pass
+
+
+class VisitorPatternTestCase(unittest.TestCase):
+    pass
+
+
+class BuilderPatternTestCase(unittest.TestCase):
+    pass
+
+
+##### END-TO-END TESTS #####
+
+class ReadXMLTestCase(unittest.TestCase):
+    pass
+
+
+class WriteXMLTestCase(unittest.TestCase):
+    pass
+
+
+class RoundTripXMLTestCase(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
